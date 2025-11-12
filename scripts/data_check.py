8ce9398484fe797e5c80cbf505acdf298f373b04
brainyjuice <>
2025-11-12 20:14:46 +0100

    added description in data_check.py for its usage

--- scripts/data_check.py
+++ scripts/data_check.py
@@ -4,6 +4,32 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 from src.dataset import GTSRBConceptDataset
 
+"""
+data_check.py
+
+Purpose:
+    basically task 1 in task sheet.
+    sanity check for the datalaoding pipeline, verify setup
+
+    applies transforms as defined in config/data_config.yaml, returns:
+    - no of samples
+    - no of concept features (K)
+    - tensor shapes for a single batch
+
+    verifies:
+    - correct image paths + extensions
+    - concepts_per_class.csv is readable
+
+Usage:
+    from project root:
+    python -m scripts.data_check
+
+Output:
+    Using concepts CSV: data/concepts_per_class.csv
+    Train samples: 39209 | num_concepts: 43
+    Batch: torch.Size([64, 3, 128, 128]) torch.Size([64, 43]) torch.Size([64])
+"""
+
 def load_cfg(path):
     with open(path, "r") as f:
         return yaml.safe_load(f)

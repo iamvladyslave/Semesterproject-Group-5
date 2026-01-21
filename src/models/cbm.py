1585ea6531a94d0db3ebed1fdbb1e0bc2b673136
KarlLehmann71 <karl-leh@gmx.de>
2026-01-21 19:07:26 +0100

    Change to Binarization

--- src/models/cbm.py
+++ src/models/cbm.py
@@ -30,26 +30,23 @@ class LabelPredictor(nn.Module):
 class CBMModel(nn.Module):
     #CBM Model bestehend aus Concept Predictor und Label Predictor
     #binary_concepts: ob die Konzepte binär sind, binary_treshold: Schwellenwert für die Binarisierung der Konzepte
-    def __init__(self, concept_predictor: nn.Module, label_predictor: nn.Module, binarization_threshold: float = 0.5, binary_concepts: bool = False):
+    def __init__(self, concept_predictor: nn.Module, label_predictor: nn.Module, binarization_threshold: float = 0.5):
         super().__init__()
         self.concept_predictor = concept_predictor
         self.label_predictor = label_predictor
         self.binarization_threshold = binarization_threshold
-        self.binary_concepts = binary_concepts
     
     def forward(self, images):
         #concept prediction
         concept_logits = self.concept_predictor(images)
         #concept activation
         concept_probs = torch.sigmoid(concept_logits)
-        #binarization der Konzepte wenn binary_concepts True ist
-        if self.binary_concepts:
-            concept_probs = (concept_probs >= self.binarization_threshold).float()
-            concepts = concept_probs
-        else:
-            concepts = concept_probs
+        #binarization
+        concepts = (concept_probs >= self.binarization_threshold).float()
+        
         #label prediction
         label_logits = self.label_predictor(concepts)
+        #concept activation
         label_probs = torch.softmax(label_logits, dim=1)
 
         return label_logits, label_probs, concept_probs, concepts
